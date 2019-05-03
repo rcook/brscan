3bff971396cd054c9f1ccb91dad652b6d3bcc355
Richard Cook <rcook@rcook.org>
2019-05-03 09:06:48 -0700

    Decode DISKINFO

--- brscan.py
+++ brscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import string
+import struct
 
 DUMP_COLUMN_WIDTH = 16
 HEADER_SIZE = 48
@@ -16,6 +17,13 @@ class Header(object):
 
     @property
     def file_type(self): return self.__file_type
+
+class DISKINFO(object):
+    def __init__(self, song_count):
+        self.__song_count = song_count
+
+    @property
+    def song_count(self): return self.__song_count
 
 def dump_line(n, idx, data):
     print("{0:08x}  ".format(idx), end="")
@@ -44,11 +52,26 @@ def dump(label, data):
         dump_line(n, idx, data)
     print("===== END {}".format(label))
 
-def fetch(n, i):
+def assert_at_end(i):
+    try:
+        next(i)
+        raise FormatError("Expected end of file")
+    except StopIteration:
+        pass
+
+def fetch_byte(i):
+    return struct.unpack("<B", next(i))[0]
+
+def fetch_16u(i):
+    b0 = fetch_byte(i)
+    b1 = fetch_byte(i)
+    return (b0 << 8) + b1
+
+def fetch_byte_string(n, i):
     return [ next(i) for _ in xrange(n) ]
 
 def fetch_str(n, i):
-    return "".join(fetch(n, i))
+    return "".join(fetch_byte_string(n, i))
 
 def fetch_header(i):
     tags = fetch_str(HEADER_SIZE, i).strip().split(" ")
@@ -63,6 +86,16 @@ def fetch_header(i):
         raise FormatError("Unsupported file type {}".format(tag3))
     return Header(tag3)
 
+def fetch_DISKINFO(i):
+    n0 = int(fetch_str(4, i))
+    n1 = fetch_16u(i)
+    if n1 != n0 + 1:
+        raise FormatError("Inconsistent song counts in DISKINFO {} vs {}".format(n0, n1))
+    if fetch_byte(i) != 0 or fetch_byte(i) != 0:
+        raise FormatError("Unexpected trailing bytes in DISKINFO")
+    assert_at_end(i)
+    return DISKINFO(n0)
+
 def read(path):
     with open(path, "rb") as f:
         data = f.read()
@@ -70,7 +103,11 @@ def read(path):
     i = iter(data)
 
     h = fetch_header(i)
-    print("file type: {}".format(h.file_type))
+    if h.file_type == "DISKINFO":
+        disk_info = fetch_DISKINFO(i)
+        print("DISKINFO: song_count={}".format(disk_info.song_count))
+    else:
+        raise RuntimeError("Not implemented")
 
 def main():
     read("DISKINF2.BR0")
